5f93c88a369b9195898b8c14470c46ae7f0ec8a8
Franyeliza <franyelizarodriguez@live.com>
2021-03-26 00:21:28 -0400

    vulnerabilities fixed

--- config.js
+++
@@ -1,5 +0,0 @@
-
-
-const CONNECTION_URL = 'mongodb+srv://FranyelizaRodriguez:Macbookprorl1122556@cluster0.p6z0v.mongodb.net/Courrier?retryWrites=true&w=majority';
-
-module.exports = CONNECTION_URL;
